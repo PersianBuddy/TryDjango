406168a758be2abf2741b9c6609b50d97c3ffa93
PersianBuddy <mnajafi0014@gmail.com>
2020-04-10 21:00:10 +0430

    Add product_detail_view and show product detail in it

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
+def product_detail_view(request, *args, **kwargs):
+    product_object = Product.objects.get(id = 2)
+    context ={
+        "product_object" : product_object,
+    }
+    return render(request,'product/detail.html',context)
