c8b487c82f4c5de4561b4c4a427d2dd5c8f73118
PersianBuddy <mnajafi0014@gmail.com>
2020-04-15 19:50:29 +0430

    Use [widgets] to change form fields attributes

--- products/forms.py
+++ products/forms.py
@@ -3,7 +3,12 @@ from django import forms
 from .models import Product
 
 class ProductCreateForm(forms.Form):
-    title = forms.CharField(max_length=100)
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+                            'placeholder': "Your Title",
+                         }))
     price = forms.DecimalField(max_digits=6, decimal_places=2)
-    summary = forms.CharField()
+    summary = forms.CharField(widget=forms.Textarea(attrs={
+                            'class': 'my-class',
+                            'placeholder': 'Insert some summary about this product',
+                        }))
 
