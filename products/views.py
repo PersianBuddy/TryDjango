7da29b28161ad04487b52c7b8ed65d50b5edb1a4
PersianBuddy <mnajafi0014@gmail.com>
2020-04-15 19:54:03 +0430

    Debug [Create_product_view] so it will create new instance of [ProductCreateForm} after making a Post request successfully

--- products/views.py
+++ products/views.py
@@ -17,6 +17,7 @@ def create_product_view(request, *args, **kwargs):
             Product.objects.create(**product_create_form.cleaned_data)
         else:
             print(product_create_form.errors)#print errors in console
+        product_create_form= ProductCreateForm()
     context = {
         'form' : product_create_form,
     }
